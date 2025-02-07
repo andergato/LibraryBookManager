7ffe1bb779b5b2b01166d8e3a66ef345f43c251e
andergato <145510193+andergato@users.noreply.github.com>
2025-02-06 20:56:26 -0600

    Added addBook method and stubs for others

--- src/edu/mu/librarybookmanager/Library.java
+++ src/edu/mu/librarybookmanager/Library.java
@@ -1,5 +1,33 @@
 package edu.mu.librarybookmanager;
 
 public class Library {
-
+	
+	private Book[] books = new Book[5];
+	private int count = 0;
+	
+	//If there is space in Book array, add book, increase count, and return true. 
+	//If there is no space, return false.
+	public boolean addBook(Book book){
+		if(count < 5) {
+			books[count] = book;
+			count++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean removeBook(Book book){
+		return true;
+	}
+	
+	public Book searchByISBN(String ISBN) {
+		return books[0];
+	}
+	
+	public void displayBooks() {
+		
+	}
+	
 }
