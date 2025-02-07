29c77fa0602ebe0dad7d6b55b4c43a560c8171d5
andergato <145510193+andergato@users.noreply.github.com>
2025-02-07 13:59:48 -0600

    Added searchByISBN and displayBooks methods to Library class

--- src/edu/mu/librarybookmanager/Library.java
+++ src/edu/mu/librarybookmanager/Library.java
@@ -1,5 +1,6 @@
 package edu.mu.librarybookmanager;
 
+
 public class Library {
 	
 	private Book[] books = new Book[5];
@@ -19,15 +20,27 @@ public class Library {
 	}
 	
 	public boolean removeBook(Book book){
+		
 		return true;
 	}
 	
+	//Simple search method for finding a book by its ISBN. 
+	//Searches from array start to finish and returns the book once it is found.
+	//If the book is not found, returns NULL
 	public Book searchByISBN(String ISBN) {
-		return books[0];
+		for(int i = 0; i < 5; i++) {
+			if(books[i].getISBN() == ISBN) {
+				return books[i];
+			}
+		}
+		return null;
 	}
 	
+	//Uses toString for each book in the library to display information about each.
 	public void displayBooks() {
-		
+		for(int i = 0; i < 5; i++) {
+			books[i].toString();
+		}
 	}
 	
 }
