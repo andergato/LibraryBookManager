5dd2fb500fe64175e7c24b30727ff4bf1e35548d
andergato <145510193+andergato@users.noreply.github.com>
2025-02-10 12:18:30 -0600

    Added Javadoc for each method. Wrote descriptions for each parameter and return as well as an explanation of what each method does.

--- src/edu/mu/librarybookmanager/Library.java
+++ src/edu/mu/librarybookmanager/Library.java
@@ -6,8 +6,16 @@ public class Library {
 	private Book[] books = new Book[5];
 	private int count = 0;
 	
-	//If there is space in Book array, add book, increase count, and return true. 
-	//If there is no space, return false.
+	/**
+	 * If there is space in Book array, add book, increase count, and return true. 
+	 * If there is no space, return false.
+	 * 
+	 * @param book
+	 * The Book you would like to add to the library
+	 * 
+	 * @return If the book is added successfully, returns true, otherwise
+	 * will return false.
+	 */
 	public boolean addBook(Book book){
 		if(count < 5) {
 			books[count] = book;
@@ -19,14 +27,29 @@ public class Library {
 		}
 	}
 	
+	
+	/**
+	 * Removes a book from the library
+	 * @param book
+	 * The Book you would like to remove from the library
+	 * @return
+	 * If the book is removed successfully, returns true, otherwise
+	 * will return false.
+	 */
 	public boolean removeBook(Book book){
-		
+		searchByISBN(book.getISBN());
 		return true;
 	}
 	
-	//Simple search method for finding a book by its ISBN. 
-	//Searches from array start to finish and returns the book once it is found.
-	//If the book is not found, returns NULL
+	/**
+	 * Simple search method for finding a book by its ISBN. 
+	   Searches from array start to finish and returns the book once it is found.
+	 * @param ISBN
+	 * The ISBN of the book you are searching for
+	 * @return
+	 * Will return the book you are looking for if it is found,
+	 * otherwise will return null. 
+	 */
 	public Book searchByISBN(String ISBN) {
 		for(int i = 0; i < 5; i++) {
 			if(books[i].getISBN() == ISBN) {
@@ -36,7 +59,9 @@ public class Library {
 		return null;
 	}
 	
-	//Uses toString for each book in the library to display information about each.
+	/**
+	 * Uses toString for each book in the library to display information about each.
+	 */
 	public void displayBooks() {
 		for(int i = 0; i < 5; i++) {
 			books[i].toString();
